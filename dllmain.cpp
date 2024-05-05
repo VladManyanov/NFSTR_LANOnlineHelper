d371058d2a47954fc5698f80db5c49d2ccf36a2c
Hypercycle <mvv0105nfs@gmail.com>
2024-05-06 01:46:30 +0700

    Playlist Session randomizer fix (wip)

--- dllmain.cpp
+++ dllmain.cpp
@@ -326,7 +326,41 @@ void ForcePlaylistSession()
 	// With Debug Playlist request, value will be randomized by server (or client?).
 	std::string sessionIDParam = ini.get(ServerCfg).get("PlaylistSessionID");
 	uint32_t sessionID = StrToULong(sessionIDParam);
-	injector::WriteMemory<uint8_t>(0x27A3304, sessionID, false);
+	//injector::WriteMemory<uint8_t>(0x27A3304, sessionID, false);
+}
+
+void SaveRandomSessionId()
+{
+	uint8_t currentSessionId = *(BYTE*)0x027A3304;
+	injector::WriteMemory<uint8_t>(0x01400182, currentSessionId, true);
+}
+
+uintptr_t playlistSessionLoaderRetPtr = 0x013FFE4B;
+__declspec(naked) void FixPlaylistSessionRandomizer_asmPart()
+{
+	_asm
+	{
+		call SaveRandomSessionId
+		jmp playlistSessionLoaderRetPtr
+	}
+}
+
+// 
+void FixPlaylistSessionRandomizer()
+{
+	if (ini.get(ServerCfg).get("ForcePlaylistLoading") != trueStr)
+	{
+		return;
+	}
+	injector::MakeNOP(0x13FFE3F, 16);
+	injector::WriteMemory<uint32_t>(0x13FFE3F, EndianSwap(0xC6868410), true);
+
+	injector::WriteMemory<uint8_t>(0x13FFE43, 0x00, true);
+	injector::WriteMemory<uint8_t>(0x13FFE44, 0x00, true);
+	injector::WriteMemory<uint8_t>(0x13FFE45, 0x01, true);
+	
+	injector::MakeJMP(0x13FFE46, FixPlaylistSessionRandomizer_asmPart);
+	injector::WriteMemory<uint32_t>(0x13FFE4B, EndianSwap(0x5EC20400), true);
 }
 
 // Skips intro movies and sequence.
@@ -450,6 +484,7 @@ void InitServerPreLoadHelper()
 	ForceCareerStatePreLoad();
 	DisableSSLCertRequirement();
 	ForceServerPlaylistLoading();
+	FixPlaylistSessionRandomizer();
 }
 
 void InitClientThreadedHelper()
